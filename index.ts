96eb3a1b18c67e5eb2779b16f8c0925a3c394ad3
Akmal Khan <akmalnawabi007@gmail.com>
2025-05-03 11:36:58 +0430

    prac

--- index.ts
+++ index.ts
@@ -50,24 +50,35 @@
 // let ab: C = {e: 'ehmady', d: 3}
 // console.log(ab)
 
-// inheritance
-class Car {
-    name: string
-    country: string
+// type a = {a: string}
+// type b = {b: number}
+// type ab = a & b
+// let c: ab = {a: 'akmal', b: 9}
+// console.log(c)
 
-    constructor (name: string, country: string){
-        this.name = name;
-        this.country = country;
-    }
-    product(){
-        console.log('car name is ' + this.name + ' from country ' + this.country)
-    }
-}
-class Benz extends Car {
-    speed(){
-        console.log('high speed car of ' + this.country)
-    }
-}
-let benz = new Benz('gClass', 'germany')
-benz.product()
-benz.speed()
+// inheritance
+// class Car {
+//     name: string
+//     country: string
+
+//     constructor (name: string, country: string){
+//         this.name = name;
+//         this.country = country;
+//     }
+//     product(){
+//         console.log('car name is ' + this.name + ' from country ' + this.country)
+//     }
+// }
+// class Benz extends Car {
+//     speed(){
+//         console.log('high speed car of ' + this.country)
+//     }
+// }
+// let benz = new Benz('gClass', 'germany')
+// benz.product()
+// benz.speed()
+
+// optional chaining
+let user = {name : 'ahmad', address : {city: 'kabul'}}
+console.log(user.address?.city)
+console.log(user.name)
